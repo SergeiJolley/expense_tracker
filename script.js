7e982b711ba1c4d6fdb0e4c6c17d5fd62591ad8a
unknown <jolley.sergei@gmail.com>
2020-07-21 12:14:18 -0600

    addedNumberString

--- script.js
+++ script.js
@@ -68,8 +68,7 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(transaction.amount
-        )}</span><button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>`;
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount).toLocaleString()}</span><button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>`;
 
     list.appendChild(item);
 }
